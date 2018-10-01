203fa0bad6871ec644845b8a12d6b2f95ec1083a
Evan Keeton <keetonnj@edison.k12.nj.us>
2018-09-30 21:02:35 -0400

    Switch to `app.js` to run file
    - Makes room for `index.js` to create an interactive webpage
      - Updates `index.html` to reference index.js
    - Track entry point change in `package.json`

---
+++ app.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const http = require('http')
+const fs = require('fs')
+
+const port = 8888
+
+console.log('Hello World!')
+
+http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '/index.html') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('./index.html', 'UTF8', (err, data) => {
+      if (err) throw err
+      res.end(data)
+    })
+  } else if (req.url === '/index.css') {
+    res.writeHead(200, { 'Content-Type': 'text/css' })
+    fs.readFile('./index.css', 'UTF8', (err, data) => {
+      if (err) throw err
+      res.end(data)
+    })
+  } else if (req.url === '/index.js') {
+    res.writeHead(200, { 'Content-Type': 'text/javascript' })
+    fs.readFile('./index.js', 'UTF8', (err, data) => {
+      if (err) throw err
+      res.end(data)
+    })
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('Page Not Found')
+  }
+}).listen(port)
+
+console.log(`Server listening on port ${port}`)
